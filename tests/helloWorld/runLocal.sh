495ffac7ce300c3b0f3daa4faa65ddf1756fe869
ted Liefeld <liefeld@broadinstitute.org>
2017-08-08 14:10:57 -0700

    switching to subtree for runLocal with this instantiation just providing the command line and container name

--- tests/helloWorld/runLocal.sh
+++ tests/helloWorld/runLocal.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
 
 TEST_ROOT=$PWD
-TASKLIB=$TEST_ROOT/src
-INPUT_FILE_DIRECTORIES=$TEST_ROOT/data
-S3_ROOT=s3://moduleiotest
-WORKING_DIR=$TEST_ROOT/job_1
-
 COMMAND_LINE="python $TASKLIB/hello.py $INPUT_FILE_DIRECTORIES/hello.txt"
-# ##### NEW PART FOR SCRIPT INSTEAD OF COMMAND LINE ################################
-# Make the input file directory since we need to put the script to execute in it
-mkdir -p $WORKING_DIR
-mkdir -p $WORKING_DIR/.gp_metadata
-EXEC_SHELL=$WORKING_DIR/.gp_metadata/local_exec.sh
+DOCKER_CONTAINER=genepattern/docker-aws-python36
 
-echo "#!/bin/bash\n" > $EXEC_SHELL
-echo $COMMAND_LINE >>$EXEC_SHELL
-echo "\n " >>$EXEC_SHELL
-
-chmod a+x $EXEC_SHELL
-
-
-REMOTE_COMMAND="runLocal.sh $TASKLIB $INPUT_FILE_DIRECTORIES $S3_ROOT $WORKING_DIR $EXEC_SHELL"
-
-echo "Container will execute $REMOTE_COMMAND  <EOM>\n"
-docker run -v $TASKLIB:$TASKLIB -v $INPUT_FILE_DIRECTORIES:$INPUT_FILE_DIRECTORIES -v $WORKING_DIR:$WORKING_DIR  liefeld/python36 $REMOTE_COMMAND
-
+. ../runLocal.sh
