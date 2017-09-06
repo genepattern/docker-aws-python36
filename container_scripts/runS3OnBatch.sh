5ec1c3eed521835dea62dc2bd9bf9276ad4c6f34
ted Liefeld <liefeld@broadinstitute.org>
2017-09-06 08:50:15 -0700

    Squashed 'common/' changes from 0fca2fc..db82c6d
    db82c6d Merge pull request #9 from genepattern/later-custom-run
    c05a59f switch execution of custom script to after the sync of the workingdir, metadatadir
    42a0c6b Merge pull request #8 from genepattern/add-memory-flag
    58c434b Debugging AffyEFC on AWS Failure when local runs fine
    7c7e1cf Merge pull request #7 from genepattern/again
    9c1a7ba fix shell syntax for second if statement (ie missing semicolon)
    d574557 Merge pull request #6 from genepattern/tweak-shell-script
    c5b06ec fix shell syntax for if statement (ie missing semicolon)
    a1ad36c Merge pull request #5 from genepattern/r2-customization-reification
    bd03662 reifying R customizations into the common script- first pass
    333e88d moving some common elements into the common subtree
    efe1bfa Merge pull request #4 from genepattern/r-library-install
    3c09f55 update for shared common scripts and now owned by genepattern
    494dd9f Merge pull request #3 from genepattern/metadata-passing-fixes
    84c072f Figuring out the details of the new passing of the metadatadir
    cdc6e84 Merge branch 'master' of https://github.com/genepattern/docker-aws-common-scripts
    620a46e update README
    de8dae3 Merge pull request #2 from genepattern/s3_root_from_python_36
    d6e9701 Adding back s3_root with default initialization from python36 container
    
    git-subtree-dir: common
    git-subtree-split: db82c6dd10b1a42adbede85475cf22a9d852cd42

--- container_scripts/runS3OnBatch.sh
+++ container_scripts/runS3OnBatch.sh
@@ -33,7 +33,6 @@ echo "1. PERFORMING AWS SYNC $S3_ROOT$TASKLIB $TASKLIB"
 aws s3 sync $S3_ROOT$TASKLIB $TASKLIB --quiet
 ls $TASKLIB
 
- 
 # copy the inputs
 mkdir -p $INPUT_FILES_DIR
 echo "2. PERFORMING aws s3 sync $S3_ROOT$INPUT_FILES_DIR $INPUT_FILES_DIR"
@@ -43,10 +42,22 @@ ls $INPUT_FILES_DIR
 # switch to the working directory and sync it up
 echo "3. PERFORMING aws s3 sync $S3_ROOT$WORKING_DIR $WORKING_DIR "
 aws s3 sync $S3_ROOT$WORKING_DIR $WORKING_DIR --quiet
-aws s3 sync $S3_ROOT$GP_METADATA_DIR $GP_METADATA_DIR --quiet
-chmod a+rwx $GP_METADATA_DIR/*
+
+echo "3a synching gp_metadata_dir"
+aws s3 sync $S3_ROOT$GP_METADATA_DIR $GP_METADATA_DIR 
 
 cd $WORKING_DIR
+echo "3b. chmodding $GP_METADATA_DIR from $PWD"
+chmod a+rwx $GP_METADATA_DIR/*
+
+
+#
+# allow customization for specific images - eg to install RLIBs
+#
+if [ -f "/usr/local/bin/runS3Batch_prerun_custom.sh" ]; then
+   . /usr/local/bin/runS3Batch_prerun_custom.sh
+fi
+
 
 # run the module
 echo "4. PERFORMING $5"
@@ -61,5 +72,10 @@ aws s3 sync $TASKLIB $S3_ROOT$TASKLIB --quiet
 echo "7. PERFORMING aws s3 sync  $GP_METADATA_DIR $S3_ROOT$GP_METADATA_DIR"
 aws s3 sync  $GP_METADATA_DIR $S3_ROOT$GP_METADATA_DIR --quiet
 
-
+#
+# allow customization for specific images - eg to save RLIBS back to S3 for reuse
+#
+if [ -f "/usr/local/bin/runS3Batch_postrun_custom.sh" ]; then
+   . /usr/local/bin/runS3Batch_postrun_custom.sh
+fi
 
